4d2c6b26df09702a94c20516158dc32ea9d069bd
redserg <red@fnbic.ru>
2013-03-13 16:24:46 +0400

    extracting regular files information

--- extracter.pl
+++ extracter.pl
@@ -1,58 +1,34 @@
 #!/usr/bin/perl
-
+use Data::Dumper;
 
 print "$ARGV[0]\n";
-open my $rf, "<" , "$ARGV[0]" or die("open error");
-%diffs;
+my $diffsdir = $ARGV[0];
+my %db;
+&extract(\$diffsdir, \%db);
+print Dumper(\%db);
 
-sub read_file_inf{
-	my ($file, $winpath, $task_id, $state)= @_;
-	my $size;
-	my $type;
-	if(-f $file){
-		$size = -s $file;
-		$type = `file -b '$file'`;
-		chop $type;
-		#($type)=$type=~/^(.{0,512})/;
-		print "'$winpath','$task_id','$file',$size,'$type','$state'\n\n";
-	}
-	if(-d $file){
-		my $len=length "$file/";
-		my @all_files=glob "$file/*";
-		for(@all_files){
-			my $name=substr($_,$len);
-			&read_file_inf($_,"$winpath/$name",$task_id, $state); 	
-		}
-	}
-}
-#!/usr/bin/perl
-require '/etc/romashka.conf';
-unshift (@INC ,$LIBROOT);
-require 'db.pl';
+print "$db{'startu.bat'}->{'path'}\n"; 			
 
-system("date");
-my $maindir="/data/romashka/postp";
-my @paths=db_get_col("select postppath from tasks where diff>0 order by id");
-my @result = sort @paths;#2012-02/00001362
-
-
-
-for my $dir(@result){
-	my %names;
-	open my $rf, "<" , "$maindir/$dir/diffs/diff.files" or next;
+sub extract{
+	my ($diffdir, $db) = @_;
+	open my $rf, "<" , "$diffsdir/diff.files" or return -1;########## 
 	while(<$rf>){
 		if(m/'(.+?)'\s(\w+)\s(\w+_\d+)$/){
-			my ($winpath, $action, $path) = ($1,$2,$3);
-			($names{$path} ->{'action'}) = $action =~ /^(.)/ if($action ne '');
-			$names{$path} ->{'winpath'} = $winpath if($winpath ne '');
+			my ($winpath, $state, $path) = ($1,$2,$3);
+			if ($winpath ne ''){
+					($db{$winpath}->{'state'}) = $state =~ /^(.)/ if($state ne '');
+					$db{$winpath}->{'path'} = $path;
+					
+			}
 		}
 	}
 	close $rf;
-	$dir =~ m#.*?/(\w*)$#;
-	my $task_id = $1;
-	for(keys %names){
-		&read_file_inf("$maindir/$dir/diffs/$_",$names{$_}->{'winpath'},$task_id,$names{$_}->{'action'});
-	}
+
+
+
+	#for(keys %names){
+	#	&read_file_inf("$diffsdir/$_",$names{$_}->{'winpath'},$task_id,$names{$_}->{'action'});
+	#}
 }
 		
 sub read_file_inf{
@@ -65,7 +41,6 @@ sub read_file_inf{
 		chop $type;
 		($type)=$type=~/^(.{0,512})/;
 		print "'$winpath','$task_id','$file',$size,'$type','$state'\n\n";
-		#db_query("insert into diffs2 (winpath,task_id,path,size,filetype,state) values (?,?,?,?,?,?)",$winpath,$task_id,$file,$size,$type,$state);
 	}
 	if(-d $file){
 		my $len=length "$file/";
@@ -76,4 +51,3 @@ sub read_file_inf{
 		}
 	}
 }
-system("date");
